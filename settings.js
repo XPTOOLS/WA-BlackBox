142987c995a9634ca6579fbc85a47332e92d66f0
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-05-28 14:23:32 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'WA BlackBox',
+  author: '‎',
+  botName: "WA BlackBox",
+  botOwner: 'FreenetHub Devz', // Your name
+  ownerNumber: '25761787221', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.1",
+};
+
+module.exports = settings;
